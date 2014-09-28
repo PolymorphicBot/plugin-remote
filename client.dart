5ad300c64333f7e8866771ad30771cd9422f7e0d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-27 23:54:33 -0400

    Line-based Client

--- client.dart
+++ client.dart
@@ -3,8 +3,8 @@ import "dart:convert";
 
 void main() {
   Socket.connect("127.0.0.1", 8031).then((socket) {
-    socket.transform(UTF8.decoder).listen((data) {
-      print(data);
+    socket.transform(UTF8.decoder).transform(new LineSplitter()).listen((String line) {
+      print(line);
     });
     
     socket.write(JSON.encode({
--- main.dart
+++ main.dart
@@ -13,7 +13,6 @@ void main(List<String> args, port) {
   print("[Remote] Loading Plugin");
   bot = new BotConnector(port);
   eventManager = bot.createEventManager();
-  
 
   runZoned(() {
     ServerSocket.bind("0.0.0.0", 8031).then((server) {
@@ -48,10 +47,10 @@ void handleSocket(Socket socket) {
     var json = JSON.decode(data);
     
     if (json is! Map) {
-      socket.write(JSON.encode({
+      client.send({
         "type": "error",
         "message": "JSON must be a map!"
-      }));
+      });
       return;
     }
     
@@ -80,7 +79,7 @@ class SocketClient {
   SocketClient(this.socket);
   
   void send(Map<String, dynamic> data) {
-    socket.write(JSON.encode(data));
+    socket.writeln(JSON.encode(data));
   }
   
   void sendEvent(Map data) {
